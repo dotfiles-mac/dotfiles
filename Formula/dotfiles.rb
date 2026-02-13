1c979fd0085a16c050a81734c055c117292443e2
Niladri Das <bniladridas@users.noreply.github.com>
2026-02-14 05:21:39 +0530

    feat: add homebrew formula

---
+++ Formula/dotfiles.rb
@@ -0,0 +1,17 @@
+class Dotfiles < Formula
+  desc "Developer toolkit with Mac setup, Git hooks, and CLI tools"
+  homepage "https://github.com/dotfiles-mac/dotfiles"
+  url "https://github.com/dotfiles-mac/dotfiles.git"
+  version "1.0.0"
+  license "MIT"
+
+  depends_on "dart" => :recommended
+
+  def install
+    bin.install "setup-mac.sh" => "dotfiles-setup"
+  end
+
+  def post_install
+    puts "\nRun 'dotfiles-setup' to set up your Mac with all development tools."
+  end
+end
